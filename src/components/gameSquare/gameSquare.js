2a01304cc50d1f23959828e6ba2bc860ef49280e
FoongL <leung.foong@gmail.com>
2023-09-23 07:08:28 +0800

    boiler plate code added into main branch

--- src/components/gameSquare/gameSquare.js
+++ src/components/gameSquare/gameSquare.js
@@ -1,34 +1,13 @@
 import "./gameSquare.css";
 import { useState, useEffect } from "react";
 
-function GameSquare({ id, changeTurn, turn, markSquare, winner, reset }) {
-  const [owner, setOwner] = useState(null);
-
-  useEffect(()=>{
-    setOwner(null)
-  },[reset])
-
-  const handleClick = () => {
-    if (!owner && !winner) {
-      console.log(`${id} has been clicked!`, turn);
-      if (turn) {
-        markSquare({ id, owner: 1 });
-        setOwner("X");
-      } else {
-        markSquare({ id, owner: 2 });
-        setOwner("O");
-      }
-      changeTurn();
-    }
-  };
+function GameSquare(props) {
+ 
 
   return (
     <div
-      className={`gameSquare square${id}`}
-      onClick={handleClick}
-      id={`square${id}`}
     >
-      <p>{owner ? owner : ""}</p>
+      <p>This is your game square component</p>
     </div>
   );
 }
