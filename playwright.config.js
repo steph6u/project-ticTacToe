ed1eaa1d345a9ab27155ab5ceacd65b1a9d211e3
neokaiyuan <kai@rocketacademy.co>
2023-11-13 17:35:24 +0800

    Write first Playwright test, disable RTL ESLint warnings on Playwright tests

--- playwright.config.js
+++ playwright.config.js
@@ -25,7 +25,9 @@ module.exports = defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    // This can be configured to be an environment variable to
+    // run same tests across dev, staging and prod.
+    baseURL: "http://127.0.0.1:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -70,9 +72,9 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: "npm run start",
+    url: "http://127.0.0.1:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
